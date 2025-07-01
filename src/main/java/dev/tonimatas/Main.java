87800a5f54fc10af54bd09df28750398c08aafc9
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-07-01 23:28:03 +0200

    feat: Initializer

--- src/main/java/dev/tonimatas/Main.java
+++ src/main/java/dev/tonimatas/Main.java
@@ -1,10 +1,13 @@
 package dev.tonimatas;
 
-import java.util.Scanner;
+import dev.tonimatas.benchmarks.*;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("""
+        System.out.println();
+        String input = JOptionPane.showInputDialog(null, """
                 Select the friendly robot what do you want to use:
                 
                 1. Aim Trainer
@@ -15,8 +18,20 @@ public class Main {
                 6. Typing
                 7. Verbal Memory
                 8. Visual Memory
-                """);
+                """, "HumanBenchmarkFriendlyRobot", JOptionPane.QUESTION_MESSAGE);
         
-        Scanner sc = new Scanner(System.in);
+        switch (input) {
+            case "1" -> new AimTrainer().run();
+            case "2" -> new ChimpTest().run();
+            case "3" -> new NumberMemory().run();
+            case "4" -> new ReactionTime().run();
+            case "5" -> new SequenceMemory().run();
+            case "6" -> new Typing().run();
+            case "7" -> new VerbalMemory().run();
+            case "8" -> new VisualMemory().run();
+            default -> {
+                System.out.println("Invalid trainer");
+            }
+        }
     }
 }
