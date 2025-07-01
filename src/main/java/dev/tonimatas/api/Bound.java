073994e33e8f3089f3ec23609f4beba7bdcc7ac7
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-07-01 22:52:15 +0200

    feat: Improve Screen util

--- src/main/java/dev/tonimatas/api/Bound.java
+++ src/main/java/dev/tonimatas/api/Bound.java
@@ -3,4 +3,7 @@ package dev.tonimatas.api;
 import java.awt.*;
 
 public record Bound(Point upperLeft, Point lowerRight) {
+    public boolean contains(Point point) {
+        return upperLeft.getX() < point.getX() && upperLeft.getY() < point.getY() && lowerRight.getX() > point.getX() && lowerRight.getY() > point.getY();
+    }
 }
--- src/main/java/dev/tonimatas/util/Screen.java
+++ src/main/java/dev/tonimatas/util/Screen.java
@@ -3,6 +3,7 @@ package dev.tonimatas.util;
 import dev.tonimatas.api.Bound;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Screen {
     public static final Color BLUE = new Color(43, 135, 209);
@@ -20,10 +21,36 @@ public class Screen {
         return new Point(centerX, centerY);
     }
     
+    public static Point getFirstPixelColor(Robot robot, Color color, Bound workingArea, Bound exclude) {
+        Dimension screenDimension = getScreenDimension();
+
+        BufferedImage screenshot = robot.createScreenCapture(new Rectangle(0, 0, screenDimension.width, screenDimension.height));
+
+        for (int x = 0; x < screenDimension.width; x++) {
+            for (int y = 0; y < screenDimension.height; y++) {
+                Color pixelColor = new Color(screenshot.getRGB(x, y));
+                if (color.equals(pixelColor)) {
+                    Point point = new Point(x, y);
+
+                    if ((exclude == null || !exclude.contains(point)) && workingArea.contains(point)) {
+                        return point;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+    
+    public static Point getFirstPixelColor(Robot robot, Color color, Bound workingArea) {
+        return getFirstPixelColor(robot, color, workingArea, new Bound(new Point(0, 0), new Point(0, 0)));
+    }
+    
     public static Bound getBlueArea(Robot robot) {
         System.out.println("Detecting area...");
         Dimension screenDimension = getScreenDimension();
 
+        BufferedImage screenshot = robot.createScreenCapture(new Rectangle(0, 0, screenDimension.width, screenDimension.height));
         Point upperLeft = null;
         Point lowerRight = null;
 
@@ -37,8 +64,8 @@ public class Screen {
 
         boolean isLast = true;
         for (int y = 0; y < screenDimension.height; y++) {
-            Color color = robot.getPixelColor(0, y);
-            robot.mouseMove(0, y);
+            Color color = new Color(screenshot.getRGB(0, y));
+
             if (color.equals(BLUE)) {
                 lowerRight = new Point(0, y);
                 isLast = false;
@@ -46,20 +73,18 @@ public class Screen {
                 if (!isLast) {
                     isLast = true;
                     for (int x = 0; x < screenDimension.width; x++) {
-                        Color color1 = robot.getPixelColor(0, y);
+                        Color color1 = new Color(screenshot.getRGB(x, y - 1));
 
-                        robot.mouseMove(x, y - 1);
                         if (color1.equals(BLUE)) {
-                            lowerRight = new Point(x, y);
+                            lowerRight = new Point(x, y - 1);
                             isLast = false;
                         } else {
                             if (!isLast) {
                                 break;
                             }
                         }
-                        
                     }
-                    
+
                     break;
                 }
             }
