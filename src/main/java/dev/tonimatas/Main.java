4755cabf1263277e118e87a1acd8c8cdea84f4c8
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-07-01 21:45:36 +0200

    feat: Primary message

--- src/main/java/dev/tonimatas/Main.java
+++ src/main/java/dev/tonimatas/Main.java
@@ -1,7 +1,22 @@
 package dev.tonimatas;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // TODO: Start click
+        System.out.println("""
+                Select the friendly robot what do you want to use:
+                
+                1. Aim Trainer
+                2. Chimp Test
+                3. Number Memory
+                4. Reaction Time
+                5. Sequence Memory
+                6. Typing
+                7. Verbal Memory
+                8. Visual Memory
+                """);
+        
+        Scanner sc = new Scanner(System.in);
     }
 }
